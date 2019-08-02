3e98cd43185febd1eb226d840afcdfe76875c308
miayang0513 <a16168972@gmail.com>
2019-08-02 21:38:49 +0800

    Delete component of hello world

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  div(id="app")
+    div(id="nav")
+      router-link(to="/about") About
 </template>
 
 <style lang="scss">
