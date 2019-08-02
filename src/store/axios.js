afcbc65d9b64550c6e40ccd91c7f326f2f8abac5
miayang0513 <a16168972@gmail.com>
2019-08-02 18:57:19 +0800

    Intialize project

---
+++ src/store/axios.js
@@ -0,0 +1,22 @@
+import Axios from 'axios'
+// import router from '../router'
+// import store from '../store/index'
+
+const options = {
+  baseURL: 'http://localhost:3000',
+  baseURL: '',
+  withCredentials: true
+}
+
+const instance = Axios.create(options)
+// config axios timeout
+instance.defaults.timeout = 5000
+/* config interceptors */
+instance.interceptors.request.use(request => {
+  return request
+})
+instance.interceptors.response.use(response => {
+  return response
+})
+
+export default instance
